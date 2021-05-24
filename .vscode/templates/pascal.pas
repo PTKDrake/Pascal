074104b2f46092ccb7a5b3deec15def9d0253f10
PTKDrake <kien192837@gmail.com>
2021-05-24 20:56:25 +0700

    Move to vscode

---
+++ .vscode/templates/pascal.pas
@@ -0,0 +1,14 @@
+(*
+ * Project: ${relativeFileDirname}
+ * Author: PTKDrake
+ * Date: ${hour}:${minute}:${second}-${day}/${month}/${year}
+ * Info: https://ad.memcvn.net
+ *)
+
+ program ${fileBasenameNoExtension};
+
+ uses windows;
+
+ begin
+    SetConsoleOutputCP(CP_UTF8);
+ end.
