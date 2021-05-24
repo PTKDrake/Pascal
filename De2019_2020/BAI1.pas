928fc009e302fac0170452137f2c7f28bb12f597
PTKDrake <kien192837@gmail.com>
2021-05-24 21:06:10 +0700

    Ok

---
+++ De2019_2020/BAI1.pas
@@ -0,0 +1,14 @@
+(*
+ * Project: De2019_2020
+ * Author: PTKDrake
+ * Date: ${hour}:${minute}:${second}-${day}/${month}/${year}
+ * Info: https://ad.memcvn.net
+ *)
+
+ program BAI1;
+
+ uses windows;
+
+ begin
+    SetConsoleOutputCP(CP_UTF8);
+ end.
