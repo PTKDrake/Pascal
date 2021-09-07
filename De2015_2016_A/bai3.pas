444daa6ad14e617c7885db97a1adcdc2aa637215
PTKDrake <kien192837@gmail.com>
2021-09-07 19:13:35 +0700

    bai3

---
+++ De2015_2016_A/bai3.pas
@@ -0,0 +1,46 @@
+(*
+ * Project: De2015_2016_A
+ * Author: PTKDrake
+ * Date: 6/9/2021
+ * Info: https://ad.mcmevn.com
+ *)
+
+program bai3;
+
+var
+  f1, f2: Text;
+  count, max, sum, i, j: LongWord;
+  arr: array of LongWord;
+  s, s2: string;
+
+begin
+    assign(f1, 'bai3.INP');
+    assign(f2, 'bai3.OUT');
+    reset(f1);
+    rewrite(f2);
+    read(f1, count, max);
+    SetLength(arr, count);
+    for i := 1 to count do read(f1, arr[i]);
+    for i := 1 to count - 1 do
+    begin
+        str(arr[i], s);
+        sum := arr[i];
+        if sum < max then
+        begin
+            for j := i + 1 to count do
+            begin
+                sum := sum + arr[j];
+                str(arr[j], s2);
+                s := s + ' ' + s2;
+                if sum <= max then 
+                begin
+                    if frac(sqrt(sum)) = 0 then writeln(f2, s);
+                end
+                else break;
+            end;
+        end
+        else if (sum = max) and (frac(sqrt(sum)) = 0) then writeln(f2, s);
+    end;
+    close(f1);
+    close(f2);
+end.
